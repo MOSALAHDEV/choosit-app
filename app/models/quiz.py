2ed97ac76f150a6ee913e32341552eb311f601f3
Mohamed Salah <mosalah525446@gmail.com>
2025-06-30 16:23:18 +0300

    feat: add Quiz model

---
+++ app/models/quiz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from app import db
+""" Quiz model """
+
+class Quiz(db.Model):
+    """ Quiz model """
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    date_of_quiz = db.Column(db.DateTime, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+    questions = db.relationship('Question', backref='quiz', lazy=True)
+    scores = db.relationship('Score', backref='quiz', lazy=True)
