16a826ce2f8bf551c99d0a0bcecc987106991517
Mohamed Salah <mosalah525446@gmail.com>
2025-06-30 16:23:18 +0300

    feat: add Score model

---
+++ app/models/score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from app import db
+from datetime import datetime
+""" Score model """
+
+class Score(db.Model):
+    """ Score model """
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
